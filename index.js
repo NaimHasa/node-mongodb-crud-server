a57c3013c8ea8c3076f7984dbdd59afded5b744a
Naim Hasan <naimarafat7@gmail.com>
2023-07-18 12:05:37 -0700

    add Deleted

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
@@ -24,15 +24,36 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
+
+
+
     try {
 
         const userCollection = client.db('NodeMongoCurd').collection('users');
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+
+        })
+
+
         app.post('/users', async (req, res) => {
             const user = req.body;
             // console.log(user);
 
             const result = await userCollection.insertOne(user)
             res.send(result);
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('trying to deleted', id)
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query)
+            res.send(result)
+
         })
 
 
